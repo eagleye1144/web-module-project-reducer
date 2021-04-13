942678291f797e9c32a8cfcdc187c84b0402e0e3
Mitchell Walker <eagleye1144@gmail.com>
2021-04-12 19:37:04 -0600

    MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY'
+export const M_PLUS = 'M_PLUS'
+export const M_REPLACE = 'M_REPLACE'
+export const M_CLEAR = 'M_CLEAR'
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
@@ -9,4 +12,24 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperator = (operator) => {
+    return({type: CHANGE_OPERATION, payload:operator })
+}
+
+export const clearDisplay = () => {
+    return ({type: CLEAR_DISPLAY})
+}
+
+export const mPlus = () => {
+    return({type: M_PLUS})
+}
+
+export const mReplace = () => {
+    return({type: M_REPLACE})
+}
+
+export const mClear = () => {
+    return({type: M_CLEAR})
 }
